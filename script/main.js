32abbfd27effc7ea0f1fa697c98b2968e46d2886
Taemin1202 <leetm1202@gmail.com>
2024-07-15 17:46:45 +0900

    1차

---
+++ script/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".main > li , .sub_bg").hover(function(){
+        $(".sub").stop().show();
+        $(".sub_bg").stop().slideDown("fast");
+    }, function(){
+        $(".sub").stop().hide();
+        $(".sub_bg").stop().slideUp("fast");
+    })/* 메뉴 호버 */
+
+    $(".move").slick({
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots:true,
+        arrows:false,
+        adaptiveHeight:true
+    })
+})/* jQ ends */
